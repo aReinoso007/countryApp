b273e80dd2d3b7c38efd50f55350aa1be0ae4b40
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-05-18 11:13:30 -0500

    router links

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -13,7 +14,8 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, 
+    RouterModule
   ], 
   exports:[
     HomePageComponent,
